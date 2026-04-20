ee82126daa65fb02b423f007282384624d8f5aec
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-20 17:42:48 +0200

    Add login endpoint to AuthController

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials-dto';
 
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -13,6 +14,7 @@ export class AuthController {
     return await this.authService.register(authCredentialsDto);
   }
 
+  
   @Post('/login')
   async login(
     @Body('password') password: string,
@@ -20,4 +22,5 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     return await this.authService.login(password, email);
   }
+  
 }
