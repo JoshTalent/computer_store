e6538c5cf9589a87a1fe2e9ed5c9cb8c7a23931e
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-20 17:43:42 +0200

    Add empty line for code readability in AuthController

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials-dto';
 
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -20,4 +21,5 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     return await this.authService.login(password, email);
   }
+  
 }
