57454aa689880118f63683f6ce123179aed743f5
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-27 21:43:02 +0200

    Update get-user.decorator.ts

--- server/src/auth/decorators/get-user.decorator.ts
+++ server/src/auth/decorators/get-user.decorator.ts
@@ -5,6 +5,7 @@ import { User } from 'src/user/interfaces/user.interface';
 export const GetUser = createParamDecorator(
   (data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
+    
     return req.user;
   },
 );
