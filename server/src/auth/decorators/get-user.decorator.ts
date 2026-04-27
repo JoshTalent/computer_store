8062cfe5fe4402d4bcbe6a46b05426e47540f95a
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-27 21:37:41 +0200

    Update get-user.decorator.ts

--- server/src/auth/decorators/get-user.decorator.ts
+++ server/src/auth/decorators/get-user.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/user/interfaces/user.interface';
 
+
 export const GetUser = createParamDecorator(
   (data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
