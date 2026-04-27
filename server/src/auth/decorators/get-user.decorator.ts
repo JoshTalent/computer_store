e30d066a7a9b9d16cd2da6e8e566a16c4a757938
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-27 22:08:45 +0200

    Update GetUser decorator implementation
    Enhance GetUser decorator to return user from request.

--- server/src/auth/decorators/get-user.decorator.ts
+++ server/src/auth/decorators/get-user.decorator.ts
@@ -2,6 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/user/interfaces/user.interface';
 
 export const GetUser = createParamDecorator(
+  
   (data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
     return req.user;
