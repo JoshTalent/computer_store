12db74cef0c6fb2c497366955d33b6c918ae19b5
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-27 22:07:55 +0200

    Remove blank line in get-user.decorator.ts
    Removed unnecessary blank line at the beginning of the file.

--- server/src/auth/decorators/get-user.decorator.ts
+++ server/src/auth/decorators/get-user.decorator.ts
@@ -1,7 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/user/interfaces/user.interface';
 
-
 export const GetUser = createParamDecorator(
   (data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
