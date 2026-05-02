2db8bb9401ef314c59fffc39f5cce61ce00d18bc
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-05-02 11:18:46 +0200

    Update useDebounce.tsx

--- frontend/src/hooks/useDebounce.tsx
+++ frontend/src/hooks/useDebounce.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 
+
 export const useDebounce = (value: string, delay: number = 60) => {
   // State and setters for debounced value
   const [debouncedValue, setDebouncedValue] = useState(value)
